bc18b725cc7e9270e2a2834715fd9a45cfc3e227
Sachman19 <sachindmishra@knights.ucf.edu>
2021-06-06 02:48:13 -0400

    Version 1

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 8 Solution
+ *  Copyright 2021 Sachin Mishra
+ */
+
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("How many people? ");
+        int people = input.nextInt();
+
+        System.out.print("How many pizzas do you have? ");
+        int pizza = input.nextInt();
+
+        System.out.print("How many slices per pizza? ");
+        int slices = input.nextInt();
+
+    }
+}
